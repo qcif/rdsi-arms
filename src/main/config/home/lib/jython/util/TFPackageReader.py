33ea755560ec9437ac202daaaefbf00993c88990
Jianfeng Li <li@redboxresearchdata.com.au>
2013-07-31 11:41:54 +0930

    Use newly added jsonUtil for accessing data
    Remove reading data using TFPackageReader.py in detail.py

--- src/main/config/home/lib/jython/util/TFPackageReader.py
+++ src/main/config/home/lib/jython/util/TFPackageReader.py
@@ -1,8 +1,32 @@
+# The RDSI - ALLOCATION REQUEST MANAGEMENT SYSTEM
+# Copyright (C) 2013 Queensland Cyber Infrastructure Foundation (http://www.qcif.edu.au/)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 class TFPackageReader:
     """
     Read the content of tfpackage. 
     tfpackage is a json file. Should be read by functions like util.preview.loadPackage.
     Methods here only deal with key:string_value pairs
+    
+    Might be deprecated: See /fascinator-common/src/main/java/com/googlecode/fascinator/common/StorageDataUtil.java for replacement
+    StorageDataUtil can be accessed in portal by $jsonUtil
+    
+    But cached self.keys might be interesting to some applications
+    Also packageJson is an internal variable to save passing it back and forth  
     """
     def __init__(self, packageJson):
         """
