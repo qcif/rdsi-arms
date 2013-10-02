efb2f628c1df705a366f527bc8ceecc56378a8b5
Jianfeng Li <li@redboxresearchdata.com.au>
2013-10-02 12:53:31 +0930

    Add switcher for users with multiple roles

--- src/main/config/portal/default/rdsi/scripts/dashboards/reviewer.py
+++ src/main/config/portal/default/rdsi/scripts/dashboards/reviewer.py
@@ -1,3 +1,21 @@
+# The RDSI - ALLOCATION REQUEST MANAGEMENT SYSTEM
+# Copyright (C) 2013 Queensland Cyber Infrastructure Foundation (http://www.qcif.edu.au/)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 from com.googlecode.fascinator.common import FascinatorHome
 import sys, os
 sys.path.append(os.path.join(FascinatorHome.getPath(), "lib", "jython", "display")) 
@@ -10,3 +28,4 @@ class ReviewerData(Dashboard):
 
     def __activate__(self, context):
         self.activate(context)
+        self.selected = "reviewer"        
