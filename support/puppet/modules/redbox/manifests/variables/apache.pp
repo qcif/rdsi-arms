ac1c06ecf94750961341b15fce37074d6a6d2f9e
mattRedbox <matt@redboxresearchdata.com.au>
2014-01-16 18:26:38 +1000

    puppet - updated syntax for proxy variables so interpreted correctly.

--- support/puppet/modules/redbox/manifests/variables/apache.pp
+++ support/puppet/modules/redbox/manifests/variables/apache.pp
@@ -2,11 +2,14 @@ class redbox::variables::apache {
 
   $enabled_mods = [ 'proxy_http', ]
 
+  $redbox_path = "http://${variables::defaults::server_id}:${variables::defaults::port}/${variables::defaults::web_path}"
+  $mint_path = "http://${variables::defaults::server_id}:9001/mint"
+
   $proxy_pass = [
     { 'path' => '/redbox',
-      'url'  => "http://${variables::defaults::server_id}:${variables::defaults:port}/redbox" },
+      'url'  => $redbox_path },
     { 'path' => '/mint',
-      'url'  => "http://${variables::defaults::server_id}:9001/mint" }
+      'url'  => $mint_path }
   ]
 
   $docroot = '/var/www/html'
