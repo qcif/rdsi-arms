e522939ae0ec554a7b3315bc46c5a61c410f743b
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2014-01-30 12:07:17 +1030

    Added environment property that is being used to show the internal authentication method when running in a dev environment.

--- src/main/config/server/tf_env.bat
+++ src/main/config/server/tf_env.bat
@@ -10,6 +10,7 @@ set ADMIN_EMAIL=${admin.email}
 set MINT_SERVER=${mint.proxy.server}
 set MINT_AMQ=${mint.amq.broker}
 set NON_PROXY_HOSTS="${non.proxy.hosts}"
+set SERVER_ENVIRONMENT=${environment}
 
 REM this script sets the environment for the fascinator scripts
 set FASCINATOR_HOME=%PROJECT_HOME%/home
@@ -65,7 +66,7 @@ REM mint integration
 set MINT_OPTS=-Dmint.proxy.server="%MINT_SERVER%" -Dmint.proxy.url="%MINT_SERVER%/mint" -Dmint.amq.broker="%MINT_AMQ%" 
 
 REM additional settings
-set EXTRA_OPTS=-Dserver.url.base="%SERVER_URL%" -Damq.port=%AMQ_PORT% -Damq.stomp.port=%AMQ_STOMP_PORT% -Dsmtp.host="%SMTP_HOST%" -Dadmin.email="%ADMIN_EMAIL%" -Dredbox.version="%REDBOX_VERSION%"
+set EXTRA_OPTS=-Dserver.url.base="%SERVER_URL%" -Dserver.environment=%SERVER_ENVIRONMENT% -Damq.port=%AMQ_PORT% -Damq.stomp.port=%AMQ_STOMP_PORT% -Dsmtp.host="%SMTP_HOST%" -Dadmin.email="%ADMIN_EMAIL%" -Dredbox.version="%REDBOX_VERSION%"
 
 REM Logging fix. Axis 1.4 (for Fedora) needs to know about the SLF4J Implementation
 set COMMONS_LOGGING=-Dorg.apache.commons.logging.LogFactory=org.apache.commons.logging.impl.SLF4JLogFactory
