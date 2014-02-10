a37d4fc49c32a6b3eccf93e18186d9ca4ca774da
mattRedbox <matt@redboxresearchdata.com.au>
2014-02-11 09:51:23 +1000

    ARMS-204. * puppet : removed index as no longer required. * added template for apache as simpler than using apache module. * updated documentation for manual, automated and shibboleth installs. * added harvest-client for time being for data migration. * added shibboleth attribute-map.xml and included in doco (TODO: incorporate in puppet install).

--- support/puppet/modules/redbox/manifests/variables/defaults.pp
+++ support/puppet/modules/redbox/manifests/variables/defaults.pp
@@ -2,12 +2,6 @@
 class redbox::variables::defaults {
   $redbox_user = 'redbox'
 
-  $port = '9000'
-	
-  $server_id = $::ipaddress
-  
-  $web_path = 'redbox/rdsi'
-
   $directories = [ 'redbox', 'mint', 'deploy', ]
 
   $static_files = [ 'deploy.sh', 'redbox.cron', 'redbox-mint.sh', 'start_all.sh'] 
