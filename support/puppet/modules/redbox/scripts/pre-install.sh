b39944741a9cb46f8c6d1218a2b88f708b1fac4e
mattRedbox <matt@redboxresearchdata.com.au>
2014-03-04 11:44:58 +1000

    ARMS-215. Added back default portal.json until default skin change solved. ARMS-285. puppet: updated readme for pre-install until r10k introduced.

--- support/puppet/modules/redbox/scripts/pre-install.sh
+++ support/puppet/modules/redbox/scripts/pre-install.sh
@@ -6,9 +6,13 @@ usage() {
 	fi
 }
 usage
+echo "Ensure you have copied the redbox module to /tmp."
 
 rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 yum install puppet
+puppet module install puppetlabs/concat
 puppet module install puppetlabs/stdlib
 puppet module install puppetlabs/apache
 
+## copy redbox module to home directory
+cp -Rf /tmp/redbox /usr/share/puppet/modules/
