a73befefeeeda2e56b15001084cc59f33ca4bc09
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2014-03-12 10:45:08 +1030

    Issues with packing startup script from rpm config. Moved configuration to post install scriptlet

--- support/rpm/rpm-build-postinst.sh
+++ support/rpm/rpm-build-postinst.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+cp /opt/redbox/server/redbox /etc/init.d/redbox
+chown root:root /etc/init.d/redbox
+chmod 755 /etc/init.d/redbox
+
 chkconfig --level 445 redbox on
 
 service redbox start
