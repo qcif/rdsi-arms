e5506129d9f1d589e319856be52e74dc2d1ba3fe
mattRedbox <matt@redboxresearchdata.com.au>
2014-03-20 12:00:37 +1000

    ARMS-315. added code for disabling and enabling submit assessment. ARMS-120. puppet. updated to check for lastest redbox. Ensure startups always through service.

--- support/puppet/modules/redbox/manifests/add_redbox_package.pp
+++ support/puppet/modules/redbox/manifests/add_redbox_package.pp
@@ -9,7 +9,7 @@ define redbox::add_redbox_package (
   $redbox_system  = $packages[system]
   $target_path    = "${install_parent_directory}/${redbox_system}/${server_directory}"
 
-  package { $redbox_package: } ->
+  package { $redbox_package: ensure => lastest, } ->
   redbox::update_server_url { $redbox_system:
     has_ssl                  => $has_ssl,
     server_url               => $server_url,
@@ -24,9 +24,8 @@ define redbox::add_redbox_package (
     cwd         => $target_path,
     subscribe   => Package[$redbox_package],
     logoutput   => true,
-  }
-
-  exec { "${target_path}/tf.sh restart":
+  } ->
+  exec { "service ${redbox_system} restart":
     tries     => 2,
     timeout   => 20,
     try_sleep => 3,
