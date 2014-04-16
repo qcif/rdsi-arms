4c47854c009a3e23d740966de9eae55b52405181
Jianfeng Li <li@redboxresearchdata.com.au>
2014-04-16 10:09:50 +0930

    Set node prefix for sequence name
    ARMS-340 #Done
    
    Also, small changes were made which should be in another commit:
    1. replace tasks with target in copy-portal-nodes
    2. remove usage of default attribute in build-nodes.xml

---
+++ support/node-management/setSequenceName.groovy
@@ -0,0 +1,21 @@
+// Replacing squence name with node specified
+
+final String VM_PATH = project.properties["dir.portal"]+ "/default/rdsi/home-components/create-request.vm"
+
+def processFileInplace(file, Closure processText) {
+	def text = file.text
+	file.write(processText(text))
+}
+
+// With a correct profile, node.list is a string with one node name  
+// In case no profile is set, make sure we are not having a list 
+def nodes = project.properties["node.list"].split(",")
+if(nodes.length > 1 ) {
+	throw new Exception("Can only be used for creating one-node package. Check profile or project's node.list.");
+}
+
+def node = nodes[0]
+def vm_file = new File(VM_PATH)
+processFileInplace(vm_file) { text ->
+	text.replaceAll(/@NODESEQUENCE@/, node)
+}
