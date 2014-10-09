6cc26fea5ef3cbde1c5c2c97d5c34b4b395311ab
Jianfeng Li <li@redboxresearchdata.com.au>
2014-10-09 14:51:13 +1030

    Setup conditions for metadata feed
    ARMSTWO-27

--- src/main/config/home/transformer-scripts/orgNotifier.groovy
+++ src/main/config/home/transformer-scripts/orgNotifier.groovy
@@ -1,31 +1,33 @@
 import com.googlecode.fascinator.transformer.ScriptingTransformer
-import com.googlecode.fascinator.common.FascinatorHome;
-import com.googlecode.fascinator.common.JsonSimple;
-import com.googlecode.fascinator.api.storage.StorageException;
+import com.googlecode.fascinator.common.FascinatorHome
+import com.googlecode.fascinator.common.JsonSimple
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+emailingConfId = "MetafeedNotifier"
+NotificationState="arms-provisioned"
+IndicationField = "metadatafeed-notification"
 
 // Send notifications to organisations listed in the contact section: 
 //   dataprovider:organization, requester:organization or nodecontact:organization.
-conf = ["dataprovider" : [
-	"org1":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"]],
-	"org2":[["type": "emailer"]],
-	"org":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"], ["type": "emailer"]]
-	],
-"requester": [
-	"org1":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"]],
-	"org2":[["type": "emailer"]],
-	"org":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"], ["type": "emailer"]]
-	],
-"nodecontact": [
-	"org1":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"]],
-	"org2":[["type": "emailer"]],
-	"org":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"], ["type": "emailer"]]
-	]
-]
+conf = ["dataprovider":[:],"requester":[:],"nodecontact":[:]]
+//conf = ["dataprovider" : [
+//	"org1":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"]],
+//	"org2":[["type": "emailer"]],
+//	"org":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"], ["type": "emailer"]]
+//	],
+//"requester": [
+//	"org1":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"]],
+//	"org2":[["type": "emailer"]],
+//	"org":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"], ["type": "emailer"]]
+//	],
+//"nodecontact": [
+//	"org1":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"]],
+//	"org2":[["type": "emailer"]],
+//	"org":[["type": "MQ", "host":"11.xx.ss.xx","port":61613,"username":"user","passcode":"pass","queuename":"q"], ["type": "emailer"]]
+//	]
+//]
 // message queue host settings should like this
 //	    "type": "MQ",
 //	    "Host": "xxx.xxx.xxx.xxx",
@@ -35,36 +37,57 @@ conf = ["dataprovider" : [
 
 log = LoggerFactory.getLogger(ScriptingTransformer.class)
 
-log.debug(conf.toString())
-
-emailingConfId = "MetafeedNotifier"
-
 oid = digitalObject.getId()
-tfp = getTfPackage()
-dataprovider = tfp.getString(null,"dataprovider:organization")
-requester = tfp.getString(null,"requester:organization")
-nodecontact = tfp.getString(null,"nodecontact:organization")
+payloads = digitalObject.getPayloadIdList()
 
-log.debug("dataprovider:organization = ${dataprovider}")
-log.debug("requester:organization = ${requester}")
-log.debug("nodecontact:organization = ${nodecontact}")
+if(payloads.contains("workflow.metadata")){
+	def workflowMeta = new JsonSimple(digitalObject.getPayload("workflow.metadata").open());
+	String workflow = workflowMeta.getString(null,"step")
+	if (workflow == NotificationState) {
+		log.debug(this.class.name + ": A transformer is set for current workflow - ${workflow}, actions may be taken.")
+		def objMetadata = digitalObject.getMetadata()
 
-Class classDef = this.class.classLoader.parseClass(new File(FascinatorHome.getPath("transformer-scripts/") + "/NotificationAgent.groovy"))
+		if (! objMetadata.getProperty(IndicationField)) {
 
-agent = classDef.newInstance()
-agent.doatest()
+			tfp = getTfPackage()
+			dataprovider = tfp.getString(null,"dataprovider:organization")
+			requester = tfp.getString(null,"requester:organization")
+			nodecontact = tfp.getString(null,"nodecontact:organization")
 
-for (c in ["dataprovider", "requester", "nodecontact"]) {
-	org = tfp.getString(null, c+":organization")
-	if (conf[c].containsKey(org)) {
-		log.debug("A ${c} notification for ${org} has been configured.")
-		notifiers = conf[c][org]
-		log.debug(notifiers.toString())
-		for (s in notifiers) {
-			notify(s)
+//			log.debug("dataprovider:organization = ${dataprovider}")
+//			log.debug("requester:organization = ${requester}")
+//			log.debug("nodecontact:organization = ${nodecontact}")
+
+			Class classDef = this.class.classLoader.parseClass(new File(FascinatorHome.getPath("transformer-scripts/") + "/NotificationAgent.groovy"))
+
+			agent = classDef.newInstance()
+
+			for (c in ["dataprovider", "requester", "nodecontact"]) {
+				org = tfp.getString(null, c+":organization")
+				if (conf[c] && conf[c].containsKey(org)) {
+					log.debug(this.class.name + ": A ${c} notification for ${org} has been configured.")
+					notifiers = conf[c][org]
+					log.debug(notifiers.toString())
+					for (s in notifiers) {
+						notify(s)
+					}
+				} else {
+					log.debug(this.class.name + ": There is no ${c} type of notification is set for ${org}.")
+				}
+			}
+
+			objMetadata.setProperty(IndicationField, "true")
+			// Set property in TF-OBJ-META
+			ByteArrayOutputStream metaOut = new ByteArrayOutputStream();
+			objMetadata.store(metaOut, "");
+			InputStream metaIn = new ByteArrayInputStream(metaOut.toByteArray());
+			digitalObject.updatePayload("TF-OBJ-META", metaIn);
+			metaIn.close();
+		} else {
+			log.debug(this.class.name + ": ${IndicationField} has been set, skip")
 		}
 	} else {
-		log.debug("There is no dataproivder type of notification is set for ${dataprovider}.")
+		log.debug(this.class.name + ": No transfomer is set for current workflow - ${workflow}, so do nothing.")
 	}
 }
 
@@ -76,8 +99,8 @@ def getTfPackage() {
 	def tfPid = ""
 	for (pid in digitalObject.getPayloadIdList()) {
 		if (pid.endsWith(".tfpackage")){
-			def payload = digitalObject.getPayload(pid);
-			tfPackage = new JsonSimple(payload.open());
+			def payload = digitalObject.getPayload(pid)
+			tfPackage = new JsonSimple(payload.open())
 		}
 	}
 	return tfPackage
@@ -89,7 +112,8 @@ void notify(s) {
 		case "MQ":
 			log.debug("grab host and other settings for MQ")
 			try {
-				agent.stomp_send(s["host"],s["port"],s["username"],s["passcode"], s["queuename"],"testing")
+				def today = new Date()
+				agent.stomp_send(s["host"],s["port"],s["username"],s["passcode"], s["queuename"],"testing: " + today)
 			} catch (Exception ex) {
 				log.error("Failed to send messaging notification for oid:" + oid, ex);
 			}
