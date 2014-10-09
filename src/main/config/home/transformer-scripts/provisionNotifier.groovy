05404d018ed47636b2f927cd4db7d74af22f4fa2
Jianfeng Li <li@redboxresearchdata.com.au>
2014-10-09 16:45:06 +1030

    Change provisionNotifier to use NotificationAgent
    ARMSTWO-27

--- src/main/config/home/transformer-scripts/provisionNotifier.groovy
+++ src/main/config/home/transformer-scripts/provisionNotifier.groovy
@@ -5,21 +5,13 @@ import com.googlecode.fascinator.common.JsonSimple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.activemq.transport.stomp.StompConnection;
-
-/*
- * TODO
- * message or emailer sending helper functions can be move to a central place: NotificationAgent.groovy
- * see orgNotifer.groovy for example
- */
-
 // message queue host settings in system-config.json should like this
 // two types are supported: MQ or emailer, it can be either one
 //	"liveArcProvisioningNotice": {
 //	    "id": "scripting",
 //	    "scriptType": "groovy",
 //	    "scriptPath": "${fascinator.home}/transformer-scripts/provisionNotifier.groovy",
-//	    "type": "MQ",
+//	    "Type": "MQ",
 //	    "Host": "xxx.xxx.xxx.xxx",
 //	    "Username": "arms",
 //	    "Passcode": "pass",
@@ -34,60 +26,48 @@ emailingConfId = "LiveArcProvisionNotifier"
 
 log = LoggerFactory.getLogger(ScriptingTransformer.class)
 
-host = null
-port = 61613
-username = null
-passcode = null
-QueueName = null
-
-notificationType = config.getString("emailer", "type")
-if (notificationType == "MQ") {
-	host = config.getString(host, "Host")
-	port = config.getInteger(port, "Port")
-	username = config.getString(username, "Username")
-	passcode = config.getString(passcode, "Passcode")
-	QueueName = config.getString(QueueName, "QueueName")
-	log.debug("host = ${host}")
-	log.debug("port = ${port}")
-	log.debug("username = ${username}")
-	log.debug("passcode = ${passcode}")
-	log.debug("QueueName = ${QueueName}")
-	if (host == null || username == null || passcode == null || QueueName == null ) {
-		log.error("Message queue error: Host, Username, Passcode and QueueName all has to be set.")
-		return digitalObject
-	}
-}
-
 payloads = digitalObject.getPayloadIdList()
-String oid = digitalObject.getId()
-def tfp = getTfPackage()
 
 if(payloads.contains("workflow.metadata")){
 	def workflowMeta = new JsonSimple(digitalObject.getPayload("workflow.metadata").open());
 	String workflow = workflowMeta.getString(null,"step")
 	if (workflow == NotificationState) {
-		log.debug("Current workflow = ${workflow}, so do something.")
+		log.debug(this.class.name + ": A transformer is set for current workflow - ${workflow}, actions may be taken.")
 		def objMetadata = digitalObject.getMetadata()
 		if (! objMetadata.getProperty(IndicationField)) {
+			def tfp = getTfPackage()
+			Class classDef = this.class.classLoader.parseClass(new File(FascinatorHome.getPath("transformer-scripts/") + "/NotificationAgent.groovy"))
+			agent = classDef.newInstance()
+
+			def notificationType = config.getString("emailer", "Type")
+//			log.debug("notificationType = ${notificationType}")
 			if (notificationType == "MQ") {
-				stomp_send(QueueName, tfp.toString())
+				def host = config.getString(null, "Host")
+				def port = config.getInteger(61613, "Port")
+				def username = config.getString(null, "Username")
+				def passcode = config.getString(null, "Passcode")
+				def queuename = config.getString(null, "QueueName")
+//				log.debug("host = ${host}")
+//				log.debug("port = ${port}")
+//				log.debug("username = ${username}")
+//				log.debug("passcode = ${passcode}")
+//				log.debug("queuename = ${queuename}")
+				if (host == null || username == null || passcode == null || queuename == null ) {
+					log.error("Message queue error: Host, Username, Passcode and QueueName all has to be set.")
+				} else {
+					agent.stomp_send(host,port,username,passcode, queuename, tfp.toString())
+					UpdateIndicationField(objMetadata)
+				}
 			} else {
-				Class emailerClass = new GroovyClassLoader(getClass().getClassLoader()).parseClass(new File(FascinatorHome.getPath("process/") + "/emailer.groovy"))
-				def emailer = emailerClass.newInstance()
-				emailer.sendNotification(emailingConfId, oid, tfp)
+				agent.sendEmail(emailingConfId, tfp)
+				// TOTO: agent may fail but it is catched, we do not know here, update may be wrong
+				UpdateIndicationField(objMetadata)
 			}
-			objMetadata.setProperty(IndicationField, "true")
-			// Set property in TF-OBJ-META
-			ByteArrayOutputStream metaOut = new ByteArrayOutputStream();
-			objMetadata.store(metaOut, "");
-			InputStream metaIn = new ByteArrayInputStream(metaOut.toByteArray());
-			digitalObject.updatePayload("TF-OBJ-META", metaIn);
-			metaIn.close();
 		} else {
 			log.debug("${IndicationField} has been set, skip")
 		}
 	} else {
-		log.debug("Current workflow = ${workflow}, so do nothing.")
+		log.debug(this.class.name + ": No transfomer is set for current workflow - ${workflow}, so do nothing.")
 	}
 }
 
@@ -106,19 +86,12 @@ def getTfPackage() {
 	return tfPackage
 }
 
-// sends message to:
-// 1. Exchange 	(AMQP default)
-// 2. Routing Key: queueName
-void stomp_send(String queueName, String message) {
-	StompConnection oConnection = new StompConnection();
-	log.debug(this.class.name + ": host = ${host}:${port}.");
-	oConnection.open(host, port);
-	oConnection.connect(username, passcode);
-	
-	log.debug(this.class.name + ": starting message sending.");
-	log.debug(this.class.name + " sending: ${message}")
-	oConnection.send("/queue/" + queueName, message, null, null);
-	log.debug(this.class.name + ": message sending completed.");
-	
-	oConnection.disconnect();
+// Set property in TF-OBJ-META
+void UpdateIndicationField(objMetadata) {
+	objMetadata.setProperty(IndicationField, "true")
+	ByteArrayOutputStream metaOut = new ByteArrayOutputStream()
+	objMetadata.store(metaOut, "")
+	InputStream metaIn = new ByteArrayInputStream(metaOut.toByteArray())
+	digitalObject.updatePayload("TF-OBJ-META", metaIn)
+	metaIn.close()
 }
